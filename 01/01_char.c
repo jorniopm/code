a31909b9aa93cc6ef3fa3e88f91de5c3a9e82b82
wangjiacheng <2984458902@qq.com>
2025-03-15 13:15:23 +0800

    更新 01_char.c 文件，添加字符变量和数组的打印输出

--- 01/01_char.c
+++ 01/01_char.c
@@ -3,10 +3,13 @@
 int main(){
     int int_1 = 80;
     printf("int_1 is %d\n", int_1);
+
     char char_1 = 'A';
     printf("char_1 is %c\n""char_1_ASCII is %d\n", char_1, char_1);
+
     char char_2 = (char)int_1;
     printf("char_2 is %c\n", char_2);
+    
     char char_array [10] = "Hello"; //char 后面的数字是数组的长度，不是字符串的长度
     printf("mem_char is %zu, %zu\n", sizeof(char_1), sizeof(char_array));
 
