a461f61daf934e53e9afa729433cbec9283f0c77
wangjiacheng <2984458902@qq.com>
2025-03-16 20:44:55 +0800

    添加 CMake 配置文件和项目文件，支持项目构建与管理

--- 01/cpl_work/ex_03.c
+++ 01/cpl_work/ex_03.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+#define MAX_N 100000
+
+int main() {
+    int count[10*MAX_N] = {0};
+    int n = 0;
+    int num = 0;
+
+    scanf("%d", &n);
+    int arr[2*n-1];
+
+    for (int i = 0; i < 2*n-1; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < 2*n-1; i++) {
+        count[arr[i]]++;
+    }
+
+    for (int i = 0; i < 2*n-1; i++) {
+        if (count[arr[i]] == 1) {
+            printf("%d", arr[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
